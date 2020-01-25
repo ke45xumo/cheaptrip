67fab1c74d6ab1d60080e81d844bbb7ed4cc06cc
cmgl <cristian.mogildea@fau.de>
2020-01-25 22:31:28 +0100

    Added helper methods for obtaining data from FuelEconomy.gov in VehicleYearActivity

--- app/src/main/java/com/example/cheaptrip/activities/VehicleYearActivity.java
+++ app/src/main/java/com/example/cheaptrip/activities/VehicleYearActivity.java
@@ -12,9 +12,18 @@ import android.widget.ListView;
 
 
 import com.example.cheaptrip.R;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.internal.LinkedTreeMap;
+import com.google.gson.stream.JsonReader;
 
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 //https://www.codeproject.com/Articles/5165136/Retrofit-A-REST-Client-for-Android-Retrofit-2-X
 //TODO: https://stackoverflow.com/questions/29380844/how-to-set-timeout-in-retrofit-library
 //TODO: Search for Vehicle in Edit Text
@@ -59,6 +68,17 @@ public class VehicleYearActivity extends ListActivity {
             carYearList.add(""+i);
         }
         return carYearList;
+
+        /*LinkedTreeMap data = getFuelEconomyData();
+        Set<String> years = getYears(data);
+        List yearsList = new ArrayList(years);
+        Collections.sort(yearsList, Collections.reverseOrder());
+        Set yearsSorted = new LinkedHashSet(yearsList);
+        //System.out.println(yearsSorted);
+
+        List<String> mainList = new ArrayList<String>();
+        mainList.addAll(yearsSorted);
+        return mainList;*/
     }
 
     @Override
@@ -71,6 +91,44 @@ public class VehicleYearActivity extends ListActivity {
         intent.putExtra("selection", selectedItem);
         setResult(Activity.RESULT_OK,intent);
         finish();
+    }
+
+    private static Set<String> getYears(LinkedTreeMap data){
+        return data.keySet();
+    }
+
+    private static LinkedTreeMap getFuelEconomyData(){
+        Object json = null;
+        try {
+            json = parse("http://data.cheaptrip.cf/vehicles.json");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        LinkedTreeMap root = (LinkedTreeMap) json;
+        LinkedTreeMap data = (LinkedTreeMap) root.get("data");
+
+        return data;
+    }
+
+    // https://javadeveloperzone.com/java-8/java-parse-large-json-file-gson-example/
+    private static Object parse(String urlString) {
+        Object o = null;
+        try {
+            URL url = new URL(urlString);
+            JsonReader jsonReader = new JsonReader(new InputStreamReader(url.openStream()));
+            Gson gson = new GsonBuilder().create();
+            jsonReader.beginArray();
+            int numberOfRecords = 0;
+            while (jsonReader.hasNext()) {
+                o = gson.fromJson(jsonReader, Object.class);
+                numberOfRecords++;
+            }
+            jsonReader.endArray();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return o;
     }
 
 }
