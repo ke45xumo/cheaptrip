82731df0cf62af3567bde615481851ccec38cf31
cmgl <cristian.mogildea@fau.de>
2020-02-01 12:53:51 +0100

    minor fixes

--- app/src/test/java/com/example/cheaptrip/ExampleUnitTest.java
+++ app/src/test/java/com/example/cheaptrip/ExampleUnitTest.java
@@ -12,21 +12,5 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-    }
 
-    @Test
-    public void test_calculateCostsForRoute(){
-        double result = RouteService.calculateCostsForRoute(20,30,1.0345);
-        double expected = 6.207;
-
-        assertEquals(result,expected,0.001);
-    }
 }
-
-   /* public static double calculateCostsForRoute( double distance , double pricePerLiter, double  avgConsumption){
-        return distance * pricePerLiter * avgConsumption/100;
-    }
-}*/
