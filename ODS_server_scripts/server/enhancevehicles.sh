f4fde1868ba3de193e96739bf90f94aaaf0ae3ff
cmgl <cristian.mogildea@fau.de>
2020-02-02 18:30:27 +0100

    minor fix

--- ODS_server_scripts/server/enhancevehicles.sh
+++ ODS_server_scripts/server/enhancevehicles.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-# Portions of this script uses the following open source software:
-# csvquote version commit baf37fa4cccc656282551db4ea7ce4ec6b9c318e, copyrighted by Dan Brown under the terms of the MIT license. The license can be downloaded from: https://github.com/dbro/csvquote/blob/master/LICENSE.md. The source code for this software is available from: https://github.com/dbro/csvquote.
+# Portions of this script use the following open source software:
+# csvquote, which is licensed under MIT license. A copy of this license can be obtained at
+# https://opensource.org/licenses/MIT. The source code for this software is available at
+# https://github.com/dbro/csvquote.
 
 mkdir -p public_html
 ./csvq public_html/vehicles.csv | cut -d, -f1-4,6-15,17-30,32-34,36-46,49-57,59-63,65-83 --complement | ./csvq -u > temp.csv
